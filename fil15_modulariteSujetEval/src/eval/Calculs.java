4b3fc13e05719fe2a5d3eb66d198b8988021dc79
anaelChardan <anael.chardan@gmail.com>
2015-12-02 14:27:06 +0100

    Second answer without test

--- fil15_modulariteSujetEval/src/eval/Calculs.java
+++ fil15_modulariteSujetEval/src/eval/Calculs.java
@@ -1,12 +1,32 @@
 package eval;
 
 import structuresAlgebriques.GroupeMul;
+import structuresAlgebriques.MonoideMul;
+
+import java.util.List;
 
 public class Calculs {
 
     //Question n1
-	public static <T extends GroupeMul<T>> T division(T x, T y)
-    {
+    public static <T extends GroupeMul<T>> T division( T x, T y ) {
         return x.produit( y.inverse() );
+    }
+
+    public static <T extends MonoideMul<T>> T produitNaire( List<T> l ) {
+        if ( l.isEmpty() ) {
+            throw new IllegalArgumentException( "La liste est vide" );
+        }
+
+        T tete = l.get( 0 );
+        if (tete != tete.un())
+        {
+            throw new IllegalArgumentException( "La liste est vide" );
+        }
+
+        for ( T e : l )
+        {
+            tete = tete.produit( e );
+        }
+        return tete;
     }
 }
