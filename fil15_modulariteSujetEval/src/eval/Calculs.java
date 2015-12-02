3c16598166aa80b0af1885e6c91d674c05ed64af
anaelChardan <anael.chardan@gmail.com>
2015-12-02 14:19:15 +0100

    First answer without test

--- fil15_modulariteSujetEval/src/eval/Calculs.java
+++ fil15_modulariteSujetEval/src/eval/Calculs.java
@@ -1,5 +1,12 @@
 package eval;
 
+import structuresAlgebriques.GroupeMul;
+
 public class Calculs {
-	// TODO
+
+    //Question n1
+	public static <T extends GroupeMul<T>> T division(T x, T y)
+    {
+        return x.produit( y.inverse() );
+    }
 }
